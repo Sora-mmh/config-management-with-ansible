93ebd5b957ed1be1817fbdde6066778dea95ef1f
Sora-mmh <montassarmhamdi80@gmail.com>
2025-07-25 16:26:37 +0200

    configure dynamic inventory for 3 ec2 servers

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,7 @@ variable ssh_key_private {}
 
 resource "aws_vpc" "montapp-vpc" { 
     cidr_block = var.vpc_cidr_block
+    enable_dns_hostnames = true
     tags = {
         Name: "${var.env_prefix}-vpc"
     }
@@ -173,16 +174,75 @@ resource "aws_instance" "montapp-server" {
     }
 }
 
-resource "null_resource" "configure_server" {
-    triggers = {
-        trigger = aws_instance.montapp-server.public_ip
-    }
+resource "aws_instance" "montapp-server-two" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
 
-    provisioner "local-exec" {
-        working_dir = "/home/montassar/devops-with-nana/ansible"
-        command = "ansible-playbook --inventory ${aws_instance.montapp-server.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml" 
+    subnet_id = aws_subnet.montapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name #"iac-server"
+
+    # user_data = <<EOF
+    #                 #!/bin/bash
+    #                 sudo yum update -y && sudo yum install -y docker
+    #                 sudo systemctl start docker
+    #                 sudo usermod -aG docker ec2-user
+    #                 docker run -p 8080:80 nginx
+    #             EOF
+
+    ### .sh scripts
+    # user_data = file("entry-script.sh")
+    # user_data_replace_on_change = true
+
+    tags = {
+        Name: "${var.env_prefix}-server-two"
     }
 }
+
+resource "aws_instance" "montapp-server-three" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.montapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name #"iac-server"
+
+    # user_data = <<EOF
+    #                 #!/bin/bash
+    #                 sudo yum update -y && sudo yum install -y docker
+    #                 sudo systemctl start docker
+    #                 sudo usermod -aG docker ec2-user
+    #                 docker run -p 8080:80 nginx
+    #             EOF
+
+    ### .sh scripts
+    # user_data = file("entry-script.sh")
+    # user_data_replace_on_change = true
+
+    tags = {
+        Name: "${var.env_prefix}-server-three"
+    }
+}
+
+
+
+### Hand control to ansible in terraform
+# resource "null_resource" "configure_server" {
+#     triggers = {
+#         trigger = aws_instance.montapp-server.public_ip
+#     }
+
+#     provisioner "local-exec" {
+#         working_dir = "/home/montassar/devops-with-nana/ansible"
+#         command = "ansible-playbook --inventory ${aws_instance.montapp-server.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml" 
+#     }
+# }
 
 ### Create a new security group (not using the default one created when creating the vpc)
 # resource "aws_security_group" "montapp-sg" {
